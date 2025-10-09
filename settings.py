c7b35a78d1131e9f3727f88c1f7c4e791e741f24
Yeasin Fiyaz <96697771+YeasinFiyaz@users.noreply.github.com>
2025-10-09 19:55:23 +0600

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+# procsim/procsim/settings.py
+INSTALLED_APPS = [
+    # ...
+    "sim",
+]
+
+# Keep default TEMPLATES, but ensure APP_DIRS=True (it is by default)
+# STATIC_URL is fine as default; you can add STATICFILES_DIRS later if needed
